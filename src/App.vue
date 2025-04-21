5f8c273163584792f4a080b7a479b68867a54bc8
raihankzh <raihanilhagusti@gmail.com>
2025-04-21 23:01:15 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* Reset atau global styling opsional */
+body {
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+  background-color: #000000;
+}
+</style>
